f31f7c1e5911c8428a5a7d7ad91811ae9767fc4f
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-23 20:42:42 +0200

    fixing

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Export all employees' TODO data to a single JSON file."""
+import json
+import requests
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    users = requests.get(base_url + "/users").json()
+    todos = requests.get(base_url + "/todos").json()
+    
+    all_data = {}
+    
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        
+        user_tasks = []
+        for task in todos:
+            if task.get("userId") == user_id:
+                user_tasks.append({
+                    "username": username,
+                    "task": task.get("title"),
+                    "completed": task.get("completed")
+                })
+        
+        all_data[str(user_id)] = user_tasks
+    
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_data, jsonfile)
