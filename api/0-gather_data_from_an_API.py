1d36dadf191a5c7c3729e74e5451991e105270fc
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-23 20:23:13 +0200

    pep8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
 
     user = requests.get(base_url + "/users/" + str(employee_id)).json()
-    todos = requests.get(base_url + "/todos", 
-                         params={"userId": employee_id}
-                         ).json()
+    todos = requests.get(base_url + "/todos",
+                         params={"userId": employee_id}).json()
 
     employee_name = user.get("name")
     total_tasks = len(todos)
@@ -19,11 +18,7 @@ if __name__ == "__main__":
     number_done = len(done_task)
 
     print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, 
-            number_done, 
-            total_tasks
-            )
-        )
+            employee_name, number_done, total_tasks))
 
     for task in done_task:
         print("\t {}".format(task.get("title")))
