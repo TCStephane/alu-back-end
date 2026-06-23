7a7eef6a6138a48570e547f084d7de191d4d1060
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-23 20:07:47 +0200

    assignment

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Export employee TODO data to CSV format."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    user = requests.get(base_url + "/users/" + str(employee_id)).json()
+    todos = requests.get(base_url + "/todos", params={"userId": employee_id}).json()
+    
+    username = user.get("username")
+    filename = str(employee_id) + ".csv"
+    
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Export employee TODO data to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    user = requests.get(base_url + "/users/" + str(employee_id)).json()
+    todos = requests.get(base_url + "/todos", params={"userId": employee_id}).json()
+    
+    username = user.get("username")
+    
+    task_list = []
+    for task in todos:
+        task_list.append({
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username
+        })
+    
+    data = {str(employee_id): task_list}
+    
+    filename = str(employee_id) + ".json"
+    with open(filename, "w") as jsonfile:
+        json.dump(data, jsonfile)
