34b3fbabb3e73ccb7095571c84e8c648a6ab4537
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-23 21:10:42 +0200

    sdjfo

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,13 +7,11 @@ import sys
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
-    
     user = requests.get(base_url + "/users/" + str(employee_id)).json()
-    todos = requests.get(base_url + "/todos", params={"userId": employee_id}).json()
-    
+    url = base_url + "/todos"
+    todos = requests.get(url, params={"userId": employee_id}).json()
     username = user.get("username")
     filename = str(employee_id) + ".csv"
-    
     with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
